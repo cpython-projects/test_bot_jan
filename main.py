20974a90099dc3eb4b507f2ff0dabe079d330c61
OST <cpython.projects@gmail.com>
2022-01-30 11:09:06 +0200

    test bot add url

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ def get_message():
 @app.route('/')
 def main():
     bot.remove_webhook()
-    bot.set_webhook(url='???????' + TOKEN)
+    bot.set_webhook(url='https://prog-kyiv-jan.herokuapp.com/' + TOKEN)
     return 'Python Telegram Bot', 200
 
 
